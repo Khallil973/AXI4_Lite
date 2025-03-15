98ac949178cbdb5a7acaa01c025cecf9cdacb9e0
Khallil973 <Khallil973>
2025-03-15 08:12:32 +0500

    working inprogress

--- src/axi4_slave.v
+++ src/axi4_slave.v
@@ -62,14 +62,7 @@ module axi4lite_slave (
                 AWREADY_S <= AWREADY_S;
             end    
 
-  /*          // =========== WRITE ADDRESS HANDSHAKE ===========
-            if (AWVALID_M && !AWREADY_S && !awaddr_valid) begin
-                AWREADY_S <= 1;
-                awaddr_reg <= AWADDR_M;
-                awaddr_valid <= 1;
-            end else begin
-                AWREADY_S <= 0;
-            end
+
 */
             // =========== WRITE DATA HANDSHAKE ===========
 
@@ -94,47 +87,6 @@ module axi4lite_slave (
             end   
 
 
-/*
-            if (WVALID_M && !WREADY_S && !wdata_valid) begin
-                WREADY_S <= 1;
-                wdata_reg <= WDATA_M;
-                wdata_valid <= 1;
-            end else begin
-                WREADY_S <= 0;
-            end
-*/
-            // =========== WRITE TRANSACTION ===========
-
-
-
-
- /*           
-            if (awaddr_valid && wdata_valid && !BVALID_S) begin
-                case (awaddr_reg)
-                    MEM_1_ADDR: mem_1 <= wdata_reg;
-                    MEM_2_ADDR: mem_2 <= wdata_reg;
-                    default: ; // Invalid
-                endcase
-                BVALID_S <= 1; // Response valid
-                BRESP_S <= 2'b00; // OKAY
-                awaddr_valid <= 0; wdata_valid <= 0;
-            end
-*/
-
-/*
-            // =========== WRITE RESPONSE ===========
-            if (BVALID_S && BREADY_M) begin
-                BVALID_S <= 0;
-            end
-
-            // =========== READ ADDRESS HANDSHAKE ===========
-            if (ARVALID_M && !ARREADY_S && !araddr_valid) begin
-                ARREADY_S <= 1;
-                araddr_reg <= ARADDR_M;
-                araddr_valid <= 1;
-            end else begin
-                ARREADY_S <= 0;
-            end
 */
             // =========== READ TRANSACTION ===========
             if (araddr_valid && !RVALID_S) begin
